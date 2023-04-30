c834c6ee73b7e5f46b945de5c37a1c845e9935e2
Joel Spadin <joelspadin@gmail.com>
2023-04-29 23:24:11 -0500

    refactor: Add a settings reset function
    For now, this just clears BLE bonds, but it can be updated in the
    future to handle clearing all settings.

--- app/src/ble.c
+++ app/src/ble.c
@@ -39,6 +39,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if IS_ENABLED(CONFIG_ZMK_BLE_PASSKEY_ENTRY)
 #include <zmk/events/keycode_state_changed.h>
+#include "ble.h"
 
 #define PASSKEY_DIGITS 6
 
@@ -353,6 +354,25 @@ int zmk_ble_set_device_name(char *name) {
         }
     }
     return update_advertising();
+}
+
+void zmk_ble_unpair_all(void) {
+    LOG_WRN("Clearing all existing BLE bond information from the keyboard");
+
+    int err = bt_unpair(BT_ID_DEFAULT, NULL);
+    if (err) {
+        LOG_ERR("Failed to unpair default identity: %d", err);
+    }
+
+    for (int i = 0; i < ZMK_BLE_PROFILE_COUNT; i++) {
+        char setting_name[15];
+        sprintf(setting_name, "ble/profiles/%d", i);
+
+        err = settings_delete(setting_name);
+        if (err) {
+            LOG_ERR("Failed to delete setting: %d", err);
+        }
+    }
 }
 
 #if IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
@@ -671,33 +691,8 @@ static void zmk_ble_ready(int err) {
 static int zmk_ble_complete_startup(void) {
 
 #if IS_ENABLED(CONFIG_ZMK_BLE_CLEAR_BONDS_ON_START)
-    LOG_WRN("Clearing all existing BLE bond information from the keyboard");
-
-    bt_unpair(BT_ID_DEFAULT, NULL);
-
-    for (int i = 0; i < 8; i++) {
-        char setting_name[15];
-        sprintf(setting_name, "ble/profiles/%d", i);
-
-        int err = settings_delete(setting_name);
-        if (err) {
-            LOG_ERR("Failed to delete setting: %d", err);
-        }
-    }
-
-    // Hardcoding a reasonable hardcoded value of peripheral addresses
-    // to clear so we properly clear a split central as well.
-    for (int i = 0; i < 8; i++) {
-        char setting_name[32];
-        sprintf(setting_name, "ble/peripheral_addresses/%d", i);
-
-        int err = settings_delete(setting_name);
-        if (err) {
-            LOG_ERR("Failed to delete setting: %d", err);
-        }
-    }
-
-#endif // IS_ENABLED(CONFIG_ZMK_BLE_CLEAR_BONDS_ON_START)
+    zmk_ble_unpair_all();
+#endif
 
     bt_conn_cb_register(&conn_callbacks);
     bt_conn_auth_cb_register(&zmk_ble_auth_cb_display);
